1e2df8c41f8eb9c72c5889dd54592c31c71c5cb0
theodesp <328805+theodesp@users.noreply.github.com>
2023-02-27 12:45:17 +0000

    Initial Commit

---
+++ components/generated/client.ts
@@ -0,0 +1,113 @@
+import {
+	Client,
+	ClientConfig,
+	CreateClientConfig,
+	User,
+	UploadRequestOptions,
+	UploadRequestOptionsWithProfile,
+	OperationMetadata,
+	OperationsDefinition,
+	OperationRequestOptions,
+	SubscriptionRequestOptions,
+	SubscriptionEventHandler,
+	FetchUserRequestOptions,
+	UploadValidationOptions,
+} from "@wundergraph/sdk/client";
+
+import type { CustomClaims } from "./claims";
+import type { PostsResponse, PostsResponseData } from "./models";
+
+export type UserRole = "admin" | "user";
+
+export const WUNDERGRAPH_S3_ENABLED = false;
+export const WUNDERGRAPH_AUTH_ENABLED = true;
+
+export enum AuthProviderId {
+	"github" = "github",
+}
+
+export interface AuthProvider {
+	id: AuthProviderId;
+	login: (redirectURI?: string) => void;
+}
+
+export const defaultClientConfig: ClientConfig = {
+	applicationHash: "6284530f",
+	baseURL: "http://localhost:9991",
+	sdkVersion: "0.137.1",
+};
+
+export const operationMetadata: OperationMetadata = {
+	Posts: {
+		requiresAuthentication: false,
+	},
+};
+
+export class WunderGraphClient extends Client {
+	query<
+		OperationName extends Extract<keyof Operations["queries"], string>,
+		Input extends Operations["queries"][OperationName]["input"] = Operations["queries"][OperationName]["input"],
+		Data extends Operations["queries"][OperationName]["data"] = Operations["queries"][OperationName]["data"]
+	>(options: OperationName extends string ? OperationRequestOptions<OperationName, Input> : OperationRequestOptions) {
+		return super.query<OperationRequestOptions, Data>(options);
+	}
+	mutate<
+		OperationName extends Extract<keyof Operations["mutations"], string>,
+		Input extends Operations["mutations"][OperationName]["input"] = Operations["mutations"][OperationName]["input"],
+		Data extends Operations["mutations"][OperationName]["data"] = Operations["mutations"][OperationName]["data"]
+	>(options: OperationName extends string ? OperationRequestOptions<OperationName, Input> : OperationRequestOptions) {
+		return super.mutate<OperationRequestOptions, Data>(options);
+	}
+	subscribe<
+		OperationName extends Extract<keyof Operations["subscriptions"], string>,
+		Input extends Operations["subscriptions"][OperationName]["input"] = Operations["subscriptions"][OperationName]["input"],
+		Data extends Operations["subscriptions"][OperationName]["data"] = Operations["subscriptions"][OperationName]["data"]
+	>(
+		options: OperationName extends string
+			? SubscriptionRequestOptions<OperationName, Input>
+			: SubscriptionRequestOptions,
+		cb: SubscriptionEventHandler<Data>
+	) {
+		return super.subscribe(options, cb);
+	}
+	public login(authProviderID: Operations["authProvider"], redirectURI?: string) {
+		return super.login(authProviderID, redirectURI);
+	}
+	public async fetchUser<TUser extends User = User<UserRole, CustomClaims>>(options?: FetchUserRequestOptions) {
+		return super.fetchUser<TUser>(options);
+	}
+}
+
+export const createClient = (config?: CreateClientConfig) => {
+	return new WunderGraphClient({
+		...defaultClientConfig,
+		...config,
+		operationMetadata,
+		csrfEnabled: true,
+	});
+};
+
+export type Queries = {
+	Posts: {
+		input?: undefined;
+		data: PostsResponseData;
+		requiresAuthentication: false;
+		liveQuery: boolean;
+	};
+};
+
+export type Mutations = {};
+
+export type Subscriptions = {};
+
+export type LiveQueries = {
+	Posts: {
+		input?: undefined;
+		data: PostsResponseData;
+		liveQuery: true;
+		requiresAuthentication: false;
+	};
+};
+
+export interface Operations
+	extends OperationsDefinition<Queries, Mutations, Subscriptions, UserRole, {}, keyof typeof AuthProviderId> {}
